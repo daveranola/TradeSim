fd6175591cabe56db73640b4d7a248ac4a47927b
DR24 <daveranola351@gmail.com>
2026-06-16 21:32:24 +0100

    initial commit

---
+++ trade-sim.cpp
@@ -0,0 +1,12 @@
+﻿// trade-sim.cpp : Defines the entry point for the application.
+//
+
+#include "trade-sim.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
